7109f3a4f25522fb419b8ccbfe3aa743f8901952
Mouad <mouad464@gmail.com>
2024-11-24 23:33:45 +0100

    style: change layout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -46,6 +46,7 @@ import { lineChartDataDashboard } from "layouts/dashboard/data/lineChartData";
 import { lineChartOptionsDashboard } from "layouts/dashboard/data/lineChartOptions";
 import { useVisionUIController } from "../../context";
 
+
 function Dashboard() {
 
   const [context , dispatch] = useVisionUIController();
@@ -54,46 +55,41 @@ function Dashboard() {
     <DashboardLayout>
       <DashboardNavbar />
       <VuiBox py={3}>
-        <VuiBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "today's money", fontWeight: "regular" }}
-                count="$53,000"
-                percentage={{ color: "success", text: "+55%" }}
-                icon={{ color: "info", component: <IoWallet size="22px" color="white" /> }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "today's users" }}
-                count="2,300"
-                percentage={{ color: "success", text: "+3%" }}
-                icon={{ color: "info", component: <IoGlobe size="22px" color="white" /> }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "new clients" }}
-                count="+3,462"
-                percentage={{ color: "error", text: "-2%" }}
-                icon={{ color: "info", component: <IoDocumentText size="22px" color="white" /> }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "total sales" }}
-                count="$103,430"
-                percentage={{ color: "success", text: "+5%" }}
-                icon={{ color: "info", component: <FaShoppingCart size="20px" color="white" /> }}
-              />
-            </Grid>
-          </Grid>
-        </VuiBox>
+
         <VuiBox mb={3}>
           <Grid container spacing="18px">
-            <Grid item xs={12} lg={12} xl={8}>
+            <Grid item xs={12} lg={12} xl={5}>
               <WelcomeMark />
+            </Grid>
+            <Grid item xl={3}>
+                <VuiBox mb={0}>
+                  <Grid direction={"column"}>
+                  <Grid item xs={12} md={6} xl={3}>
+                    <VuiBox mb={4}><MiniStatisticsCard
+                      title={{ text: "today's users" }}
+                      count="2,300"
+                      percentage={{ color: "success", text: "+3%" }}
+                      icon={{ color: "info", component: <IoGlobe size="22px" color="white" /> }}
+                    /></VuiBox>
+                  </Grid>
+                  <Grid item xs={12} md={6} xl={3}>
+                    <VuiBox mb={4}><MiniStatisticsCard
+                      title={{ text: "new clients" }}
+                      count="+3,462"
+                      percentage={{ color: "error", text: "-2%" }}
+                      icon={{ color: "info", component: <IoDocumentText size="22px" color="white" /> }}
+                    /></VuiBox>
+                  </Grid>
+                  <Grid item xs={12} md={6} xl={3}>
+                    <VuiBox mb={4}><MiniStatisticsCard
+                      title={{ text: "total sales" }}
+                      count="$103,430"
+                      percentage={{ color: "success", text: "+5%" }}
+                      icon={{ color: "info", component: <FaShoppingCart size="20px" color="white" /> }}
+                    /></VuiBox>
+                  </Grid>
+                </Grid>
+                </VuiBox>
             </Grid>
             <Grid item xs={12} lg={6} xl={4}>
               <SatisfactionRate />
